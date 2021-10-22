9761c37ccfb64df9526218024f4456adf6001c33
Bruno Wacemberg <brunowacemberg@gmail.com>
2021-10-22 08:11:01 -0300

    Adjustments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import 'element-closest-polyfill';
-import 'element-remove';
 import FormValidator from "./src/FormValidator";
 
 export default FormValidator
